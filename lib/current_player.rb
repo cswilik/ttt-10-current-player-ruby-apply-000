f01f4e4d4b145286de8039b0b41477d2af755b4c
Chelsey <cswilik@gmail.com>
2020-09-27 23:03:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   counter = 0 
   board.each do |turns|
     if turns == "X" || turns == "O"
-      counter = counter += 1
+      counter += 1
     end 
   end
   return counter
